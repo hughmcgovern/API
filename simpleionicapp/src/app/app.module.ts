cca9b0ca3d1308c08a83ecbdff7f593992c5a9d3
mcgovernh <mcgovern1969@gmail.com>
2023-04-05 15:08:09 +0000

    complies but home is not showing

--- simpleionicapp/src/app/app.module.ts
+++ simpleionicapp/src/app/app.module.ts
@@ -6,11 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {IonicStorageModule} from '@ionic/storage-angular';
+import { GlobalComponent } from './global/global.component';
+import {HttpClient} from '@angular/common/http'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    IonicStorageModule.forRoot(),
+    AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+              GlobalComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- simpleionicapp/src/app/home/home.page.ts
+++ simpleionicapp/src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Storage} from '@ionic/storage-angular';
+import { GlobalComponent } from '../global/global.component';
+import {Router} from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  req_username:any;
+  req_param:any;
+  global:any;
+  data: any;
 
-  constructor() {}
+  constructor(
+    public router:Router,
+    public globalComponent:GlobalComponent,
+    private storage:Storage,
+    public http:HttpClient) 
+    {
+      this.global = this.globalComponent;
+    }
+
     myVariable: string ="The force is with me!";
     myVariable2: string ="Hello from CCT";
     
@@ -16,5 +32,22 @@ export class HomePage {
     }
     updateMyValue2(){
       this.myVariable2 = "Hello from Ionic Mobile Development";
+    }
+
+    loadData(){
+      let url = "https://regres.in/api/users?page=1";
+      this.data = this.http.get(url);
+      this.data.subscribe((data: any)=>{
+        console.log("response",data);
+      })
+    }
+
+    async getStorageData(){}
+
+    async ngOnInit(){
+      await this.storage.create();
+
+      // with storage
+      this.getStorageData();
     }
 }
